978a61f425957e9ce4cd70d90a010aac86cffa8f
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-06-19 22:42:02 +0300

    working with Number() and unary plus

---
+++ js/toNumber.js
@@ -0,0 +1,153 @@
+//TO NUMBER
+
+//----------------String to Number
+let value = "Быть или не быть!?";
+
+//1 way Number()
+let toNumber = Number(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, typeof toNumber);
+
+//2 way unary plus
+toNumber = +value;
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО через унарний плюс -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО унарний плюс -- `, typeof toNumber);
+
+//----------------String with number to Number
+value = "333";
+
+//1 way Number()
+toNumber = Number(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, typeof toNumber);
+
+//2 way unary plus
+toNumber = +value;
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО через унарний плюс -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО унарний плюс -- `, typeof toNumber);
+
+//----------------Empty string to Number
+value = "";
+
+//1 way Number()
+toNumber = Number(value);
+console.log(`Значення змінної value - пустий рядок`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, typeof toNumber);
+
+//2 way unary plus
+toNumber = +value;
+console.log(`Значення змінної value - пустий рядок`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО через унарний плюс -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО унарний плюс -- `, typeof toNumber);
+
+//----------------String with space to Number
+value = " ";
+
+//1 way Number()
+toNumber = Number(value);
+console.log(`Значення змінної value - рядок з пробілом`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, typeof toNumber);
+
+//2 way unary plus
+toNumber = +value;
+console.log(`Значення змінної value - рядок з пробілом`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО через унарний плюс -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО унарний плюс -- `, typeof toNumber);
+
+//----------------Boolean (logic) TRUE to Number
+value = true;
+
+//1 way Number()
+toNumber = Number(value);
+console.log(`Значення змінної value `, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, typeof toNumber);
+
+//2 way unary plus
+toNumber = +value;
+console.log(`Значення змінної value `, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО через унарний плюс -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО унарний плюс -- `, typeof toNumber);
+
+//----------------Boolean (logic) FALSE to Number
+value = false;
+
+//1 way Number()
+toNumber = Number(value);
+console.log(`Значення змінної value `, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, typeof toNumber);
+
+//2 way unary plus
+toNumber = +value;
+console.log(`Значення змінної value `, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО через унарний плюс -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО унарний плюс -- `, typeof toNumber);
+
+//----------------undefined to Number
+value = undefined;
+
+//1 way Number()
+toNumber = Number(value);
+console.log(`Значення змінної value `, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, typeof toNumber);
+
+//2 way unary plus
+toNumber = +value;
+console.log(`Значення змінної value `, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО через унарний плюс -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО унарний плюс -- `, typeof toNumber);
+
+//----------------null to Number
+value = null;
+
+//1 way Number()
+toNumber = Number(value);
+console.log(`Значення змінної value `, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО з допомогою конструктора Number() -- `, typeof toNumber);
+
+//2 way unary plus
+toNumber = +value;
+console.log(`Значення змінної value `, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип ЧИСЛО через унарний плюс -- `, toNumber);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип ЧИСЛО унарний плюс -- `, typeof toNumber);
