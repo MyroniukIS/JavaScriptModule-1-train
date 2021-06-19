5e2cffed9c2e2d7139639c1af8e65e470b2c45c6
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-06-19 21:41:21 +0300

    working with String() and concatenation

---
+++ js/toString.js
@@ -0,0 +1,153 @@
+//TO STRING
+
+//----------------Number 0 to String
+let value = 0;
+
+//1 way String()
+let toString = String(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, typeof toString);
+
+//2 way concatenation
+toString = value + "";
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, typeof toString);
+
+//----------------Number 1 to String
+value = 1;
+
+//1 way String()
+toString = String(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, typeof toString);
+
+//2 way concatenation
+toString = value + "";
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, typeof toString);
+
+//----------------Infinity to String
+value = Infinity;
+
+//1 way String()
+toString = String(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, typeof toString);
+
+//2 way concatenation
+toString = value + "";
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, typeof toString);
+
+//----------------NaN to String
+value = NaN;
+
+//1 way String()
+toString = String(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, typeof toString);
+
+//2 way concatenation
+toString = value + "";
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, typeof toString);
+
+//----------------Boolean (logic) TRUE to String
+value = true;
+
+//1 way String()
+toString = String(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, typeof toString);
+
+//2 way concatenation
+toString = value + "";
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, typeof toString);
+
+//----------------Boolean (logic) FALSE to String
+value = false;
+
+//1 way String()
+toString = String(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, typeof toString);
+
+//2 way concatenation
+toString = value + "";
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, typeof toString);
+
+//----------------undefined to String
+value = undefined;
+
+//1 way String()
+toString = String(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, typeof toString);
+
+//2 way concatenation
+toString = value + "";
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, typeof toString);
+
+//----------------null to String
+value = null;
+
+//1 way String()
+toString = String(value);
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК з допомогою конструктора String() -- `, typeof toString);
+
+//2 way concatenation
+toString = value + "";
+console.log(`Значення змінної value`, value);
+console.log(`Значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, toString);
+console.log(`Тип даних значення ${value}, змінної value,
+приведене в тип РЯДОК через конкатенацію -- `, typeof toString);
